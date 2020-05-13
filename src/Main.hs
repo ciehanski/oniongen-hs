62a7e3c2020c27939d437adf823b0b11be784b3c
Ryan Ciehanski <ryan@ciehanski.com>
2020-05-13 04:44:52 +0000

    basic functionallity (#1)
    * fucntionally almost working
    
    * finally working somewhat
    
    * add concurrency for multiple address generation
    
    * space efficient processing for onionAddress thunk
    
    * cleanup

--- src/Main.hs
+++ src/Main.hs
@@ -1,47 +1,83 @@
-{-# LANGUAGE OverloadedStrings #-}
+-- Copyright (C) 2020 Ryan Ciehanski. All Rights Reserved.
 module Main where
 
-import Crypto.Sign.Ed25519 as Ed
-import Crypto.Hash
-import System.IO as Sys
-import qualified Data.ByteString as B
-import Data.ByteString.Char8
-import Data.ByteString.Base32 as Base32
-import Data.Byteable
-import System.Environment   
+import           Control.Concurrent
+import qualified Crypto.Hash            as H
+import           Crypto.Sign.Ed25519
+import           Data.Binary.Put
+import qualified Data.ByteString        as B
+import           Data.ByteString.Base32
+import qualified Data.ByteString.Char8  as C
+import qualified Data.ByteString.Lazy   as BL
+import           Data.Char
+import           System.Directory       (createDirectoryIfMissing)
+import           System.Environment     (getArgs)
+import qualified System.IO              as Sys
+import           Text.Regex.TDFA
 
 main :: IO ()
 main = do
   -- Get arguments
   args <- getArgs
-  mapM_ Sys.putStrLn args
 
-  -- Create an Ed25519 keypair, but only retain the public key
-  (pk, _) <- Ed.createKeypair
-  
-  -- Generate an onion checksum with public key
-  let chksum = genChecksum pk
+  -- Compile regex from 1st argument provided
+  let rgx = args !! 1 :: String
 
-  -- Generate onion address from public key and cheksum
-  let onionAddress = genOnionAddress pk chksum
+  -- TODO Get number of addresses to generate from 2nd argument provided
+  --let addrNum = read $ args !! 2 :: Int
 
-  Sys.putStrLn onionAddress
+  -- Buffer of valid onion addresses found
+  onionAddress <- newEmptyMVar
 
-genChecksum :: PublicKey -> ByteString
-genChecksum pk = do
+  -- Run this bad boy in another thread
+  Sys.putStrLn "Generating..."
+  forkIO $ generate onionAddress rgx
+
+  -- Wait 'til generate is complete then print the result
+  C.putStrLn =<< takeMVar onionAddress
+
+generate :: MVar C.ByteString -> String -> IO ()
+generate addr rgx = do
+  -- Create an Ed25519 keypair
+  (pk, sk) <- createKeypair
+
+  -- Generate onion address from public key and checksum
+  let onionAddress = genOnionAddress pk $ genChecksum pk
+
+  -- Compare onion address with regex argument
+  -- If no match recursively call the generate function
+  if onionAddress =~ rgx :: Bool
+    then do
+        saveKeypairs (B.concat [onionAddress, C.pack ".onion"]) rgx pk sk
+        putMVar addr $! B.concat [onionAddress, C.pack ".onion"]
+    else
+        generate addr rgx
+
+saveKeypairs :: B.ByteString -> String -> PublicKey -> SecretKey -> IO ()
+saveKeypairs onionAddress rgx pk sk = do
+  createDirectoryIfMissing True "oniongen"
+  B.writeFile ("oniongen/" ++ rgx ++ ".pub") $ unPublicKey pk
+  B.writeFile ("oniongen/" ++ rgx) $ unSecretKey sk
+  B.writeFile ("oniongen/" ++ rgx ++ ".onion") onionAddress
+
+serializeChecksum :: PublicKey -> Put
+serializeChecksum pk = do
   -- checksum = H(".onion checksum" || pubkey || version)
-  let onion = ".onion checksum" :: ByteString
-  let pub = unPublicKey pk
-  let ver = "0x03" :: ByteString
-  let chkStr = B.concat [onion, pub, ver]
-  let chksum = hash chkStr :: Digest SHA3_256 
-  toBytes chksum
+  putStringUtf8 ".onion checksum"
+  putByteString $ unPublicKey pk
+  putInt8 3
 
-genOnionAddress :: PublicKey -> ByteString -> Base32
-genOnionAddress pk chksum = do
+genChecksum :: PublicKey -> B.ByteString
+genChecksum pk = B.concat $ BL.toChunks $ runPut $ serializeChecksum pk
+
+serializeAddress :: PublicKey -> B.ByteString -> Put
+serializeAddress pk chksum = do
   -- onion_address = base32(pubkey || checksum || version)
-  let pub = unPublicKey pk
-  let ver = "0x03" :: ByteString
-  let addr = B.concat [pub, chksum, ver]
-  let onionAddr = B.unpack addr
-  Base32.encode onionAddr
+  putByteString $ unPublicKey pk
+  putByteString $ B.take 2 chksum
+  putInt8 3
+
+genOnionAddress :: PublicKey -> B.ByteString -> B.ByteString
+genOnionAddress pk chksum =
+  let addr = B.concat $ BL.toChunks $ runPut $ serializeAddress pk chksum
+   in C.map toLower $ encodeBase32' addr
